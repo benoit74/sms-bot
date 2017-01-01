bd1cc0f63c495e60b77af1c76ccfad8ce0a7c2ca
Benoit Beraud <bberaud@octo.com>
2017-01-01 20:34:34 +0100

    Validate twilio request - basics

---
+++ app/helpers/twilio_helper.rb
@@ -0,0 +1,27 @@
+require 'twilio-ruby'
+
+module TwilioHelper
+
+
+=begin
+# Get your Auth Token from https://www.twilio.com/console
+auth_token = 'your_auth_token'
+
+url = 'https://mycompany.com/myapp.php?foo=1&bar=2'
+params = {
+  'CallSid' => 'CA1234567890ABCDE',
+  'Caller'  => '+14158675309',
+  'Digits'  => '1234',
+  'From'    => '+14158675309',
+  'To'      => '+18005551212'
+}
+# The X-Twilio-Signature header attached to the request
+twilio_signature = 'RSOYDt4T1cUTdK1PDd93/VVr8B8='
+=end
+
+    def validate_request(auth_token, url, params, signature)
+        validator = Twilio::Util::RequestValidator.new(auth_token)
+        return validator.validate(url, params, twilio_signature)
+    end
+
+end
