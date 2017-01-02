ee297ac2e564400c390f953a709be63511fe15d7
Benoit Beraud <bberaud@octo.com>
2017-01-02 09:08:27 +0100

    Validate Twilio signature when request arrives

--- app/controllers/incoming_sms_controller.rb
+++ app/controllers/incoming_sms_controller.rb
@@ -3,7 +3,11 @@ require 'twilio-ruby'
 class IncomingSmsController < ApplicationController
 
     def create
-        respond_ok
+        if !TwilioHelper.validate_request(ENV['TWILIO_AUTH_TOKEN'], request.original_url, request.request_parameters, request.headers["HTTP_X_TWILIO_SIGNATURE"])
+            respond_nok
+        else
+            respond_ok
+        end
     end
 
     # Create an empty response, and return status 200
@@ -12,4 +16,12 @@ class IncomingSmsController < ApplicationController
         response = Twilio::TwiML::Response.new
         render xml:response.text, status: 200
     end
+
+    # Create an empty response, and return status 403
+    # Something went wront
+    def respond_nok
+        response = Twilio::TwiML::Response.new
+        render xml:response.text, status: 401
+    end
+
 end
